25d3b6e82fb5863c83ca3549232dd2c5140e3987
Gazi Sazid Raiyan <raiyansazid5@gmail.com>
2026-02-24 09:14:08 +0600

    after updation

--- server.c
+++ server.c
@@ -1,77 +1,57 @@
 // server.c
+// Multi-client chat server using Winsock (Improved Programming Style)
+
 #define _WIN32_WINNT 0x0601
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <process.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #pragma comment(lib, "Ws2_32.lib")
 
-#define DEFAULT_PORT "27015"
-#define DEFAULT_BUFLEN 512
-#define MAX_CLIENTS 10
+#define DEFAULT_PORT        "27015"
+#define DEFAULT_BUFLEN      512
+#define MAX_CLIENTS         10
 
 typedef struct {
     SOCKET socket;
     int id;
-} CLIENT;
+} client_t;
 
-CLIENT* clients[MAX_CLIENTS];
-int clientCount = 0;
+/* ===================== GLOBAL VARIABLES ===================== */
 
-void broadcast(const char* message, int senderId) {
-    for (int i = 0; i < clientCount; ++i) {
-       
-        send(clients[i]->socket, message, (int)strlen(message), 0);
+client_t* g_clients[MAX_CLIENTS];
+int g_client_count = 0;
+
+/* ===================== FUNCTION DECLARATIONS ===================== */
+
+int initialize_winsock(void);
+SOCKET create_listening_socket(void);
+void accept_clients(SOCKET listen_socket);
+void broadcast_message(const char* message);
+unsigned __stdcall client_thread(void* arg);
+void remove_client(int client_id);
+
+/* ===================== FUNCTION DEFINITIONS ===================== */
+
+/* Initializes Winsock library */
+int initialize_winsock(void)
+{
+    WSADATA wsa_data;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
+        printf("WSAStartup failed.\n");
+        return 0;
     }
+    return 1;
 }
 
-unsigned int __stdcall ClientThread(void* arg) {
-    CLIENT* client = (CLIENT*)arg;
-    char recvbuf[DEFAULT_BUFLEN];
-    int iResult;
-
-    printf("Client %d connected.\n", client->id);
-
-    while (1) {
-        ZeroMemory(recvbuf, DEFAULT_BUFLEN);
-        iResult = recv(client->socket, recvbuf, DEFAULT_BUFLEN, 0);
-        if (iResult > 0) {
-            printf("Client %d: %s\n", client->id, recvbuf);
-
-            char msgWithId[DEFAULT_BUFLEN + 50];
-            sprintf(msgWithId, "Client %d: %s", client->id, recvbuf);
-
-            broadcast(msgWithId, client->id);
-        } else {
-            printf("Client %d disconnected.\n", client->id);
-            break;
-        }
-    }
-
-    closesocket(client->socket);
-
-    // Remove client
-    for (int i = 0; i < clientCount; i++) {
-        if (clients[i]->id == client->id) {
-            for (int j = i; j < clientCount - 1; j++)
-                clients[j] = clients[j + 1];
-            clientCount--;
-            break;
-        }
-    }
-
-    free(client);
-    return 0;
-}
-
-int main() {
-    WSADATA wsaData;
-    SOCKET ListenSocket = INVALID_SOCKET;
-    struct addrinfo *result = NULL, hints;
-
-    WSAStartup(MAKEWORD(2,2), &wsaData);
+/* Creates, binds and listens on server socket */
+SOCKET create_listening_socket(void)
+{
+    struct addrinfo hints, *result = NULL;
+    SOCKET listen_socket = INVALID_SOCKET;
 
     ZeroMemory(&hints, sizeof(hints));
     hints.ai_family = AF_INET;
@@ -79,46 +59,158 @@ int main() {
     hints.ai_protocol = IPPROTO_TCP;
     hints.ai_flags = AI_PASSIVE;
 
-    getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
-    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-    bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
-    listen(ListenSocket, SOMAXCONN);
+    if (getaddrinfo(NULL, DEFAULT_PORT, &hints, &result) != 0) {
+        printf("getaddrinfo failed.\n");
+        return INVALID_SOCKET;
+    }
 
-    printf("Server waiting for clients...\n");
+    listen_socket = socket(result->ai_family,
+                           result->ai_socktype,
+                           result->ai_protocol);
+
+    if (listen_socket == INVALID_SOCKET) {
+        printf("Socket creation failed.\n");
+        freeaddrinfo(result);
+        return INVALID_SOCKET;
+    }
+
+    if (bind(listen_socket, result->ai_addr,
+             (int)result->ai_addrlen) == SOCKET_ERROR) {
+        printf("Bind failed.\n");
+        freeaddrinfo(result);
+        closesocket(listen_socket);
+        return INVALID_SOCKET;
+    }
+
+    freeaddrinfo(result);
+
+    if (listen(listen_socket, SOMAXCONN) == SOCKET_ERROR) {
+        printf("Listen failed.\n");
+        closesocket(listen_socket);
+        return INVALID_SOCKET;
+    }
+
+    return listen_socket;
+}
+
+/* Sends message to all connected clients */
+void broadcast_message(const char* message)
+{
+    for (int i = 0; i < g_client_count; ++i) {
+        send(g_clients[i]->socket,
+             message,
+             (int)strlen(message),
+             0);
+    }
+}
+
+/* Removes disconnected client from global list */
+void remove_client(int client_id)
+{
+    for (int i = 0; i < g_client_count; ++i) {
+        if (g_clients[i]->id == client_id) {
+            for (int j = i; j < g_client_count - 1; ++j) {
+                g_clients[j] = g_clients[j + 1];
+            }
+            g_client_count--;
+            break;
+        }
+    }
+}
+
+/* Thread function to handle a connected client */
+unsigned __stdcall client_thread(void* arg)
+{
+    client_t* client = (client_t*)arg;
+    char recv_buffer[DEFAULT_BUFLEN];
+    int result;
+
+    printf("Client %d connected.\n", client->id);
 
     while (1) {
-        SOCKET ClientSocket = accept(ListenSocket, NULL, NULL);
-        if (ClientSocket != INVALID_SOCKET) {
-            CLIENT* client = (CLIENT*)malloc(sizeof(CLIENT));
-            client->socket = ClientSocket;
-            client->id = clientCount + 1;
-            clients[clientCount++] = client;
-            _beginthreadex(NULL, 0, ClientThread, client, 0, NULL);
+        ZeroMemory(recv_buffer, DEFAULT_BUFLEN);
+
+        result = recv(client->socket,
+                      recv_buffer,
+                      DEFAULT_BUFLEN,
+                      0);
+
+        if (result > 0) {
+            printf("Client %d: %s\n",
+                   client->id,
+                   recv_buffer);
+
+            char message[DEFAULT_BUFLEN + 50];
+
+            snprintf(message,
+                     sizeof(message),
+                     "Client %d: %s",
+                     client->id,
+                     recv_buffer);
+
+            broadcast_message(message);
+        }
+        else {
+            printf("Client %d disconnected.\n",
+                   client->id);
+            break;
         }
     }
 
-    closesocket(ListenSocket);
-    WSACleanup();
+    closesocket(client->socket);
+    remove_client(client->id);
+    free(client);
+
     return 0;
 }
 
+/* Accepts incoming client connections */
+void accept_clients(SOCKET listen_socket)
+{
+    while (1) {
+        SOCKET client_socket =
+            accept(listen_socket, NULL, NULL);
 
-// ./server.exe
- 
-// gcc server.c -o server.exe -lws2_32
+        if (client_socket != INVALID_SOCKET &&
+            g_client_count < MAX_CLIENTS) {
 
+            client_t* new_client =
+                (client_t*)malloc(sizeof(client_t));
 
+            new_client->socket = client_socket;
+            new_client->id = g_client_count + 1;
 
+            g_clients[g_client_count++] = new_client;
 
+            _beginthreadex(NULL,
+                           0,
+                           client_thread,
+                           new_client,
+                           0,
+                           NULL);
+        }
+    }
+}
 
+/* ===================== MAIN FUNCTION ===================== */
 
+int main(void)
+{
+    if (!initialize_winsock())
+        return 1;
 
+    SOCKET listen_socket =
+        create_listening_socket();
 
+    if (listen_socket == INVALID_SOCKET)
+        return 1;
 
+    printf("Server waiting for clients...\n");
 
+    accept_clients(listen_socket);
 
+    closesocket(listen_socket);
+    WSACleanup();
 
-
-
-
-
+    return 0;
+}
